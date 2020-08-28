d1cee7316dc3b4e78b1eed7e3d5fdb3bc8e1d19f
Brennan <brennanbutler01@gmail.com>
2020-08-28 12:22:30 -0700

    working links, working about and contact routes. partials structure

--- app.js
+++ app.js
@@ -16,9 +16,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.render('home', {homeContent : homeStartingContent});
+  res.render('home', {homeStartingContent : homeStartingContent});
 })
-
+app.get('/about', (req, res) => {
+  res.render('about', { aboutStartingContent: aboutContent });
+})
+app.get('/contact', (req, res) => {
+  res.render('contact', { contactStartingContent: contactContent });
+})
 
 
 
